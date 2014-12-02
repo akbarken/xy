05fb7b8b973e2c250505e5fd843049e07d3567da
ken <nozuonodie_ken@163.com>
2014-12-03 00:04:56 +0800

    db

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,7 +2,7 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'mysql:host=localhost;dbname=xg',
+	'connectionString' => 'mysql:host=localhost;dbname=xy',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => 'root',
